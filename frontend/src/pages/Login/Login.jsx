e1a3fda69fa20de3d88d9d0151645687b12bf8fb
DulinaDineth1589 <dulinadineth2021@gmail.com>
2024-10-12 17:29:47 +0530

    login page's  html part created

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import './login.css';
 function Login() {
     return (
         <div>
-            <h1>Welcome to the Login Page</h1>
+            
+            <div class="login-container">
+          <h2>Login</h2>
+          <form action="#" method="post">
+            <div class="input-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" required>
+                </input>
+            </div>
+            <div class="input-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required>
+                </input>
+            </div>
+            <button type="submit">Login</button>
+         </form>
+         </div>
         </div>
     )
 }
