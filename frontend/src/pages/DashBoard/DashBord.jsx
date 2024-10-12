834d9a662440ea79911252a863d2a00f557e2b4b
Kelum-Tharindu <155412271+Kelum-Tharindu@users.noreply.github.com>
2024-10-12 12:58:16 +0530

    testing

--- frontend/src/pages/DashBoard/DashBord.jsx
+++ frontend/src/pages/DashBoard/DashBord.jsx
@@ -1,10 +1,15 @@
 import react from 'react';
+import Footer from '../../Components/Footer/Footer';
 
 function DashBord() {
     return (
-        <div>
-            <h1>Welcome to the DashBord Page</h1>
-        </div>
+        <>
+            
+            <div>
+                <h1>Welcome to the DashBord Page</h1>
+            </div>
+            <Footer />
+        </>
     )
 }
 export default DashBord
